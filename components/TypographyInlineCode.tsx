cdc7f0f32c0cdf3d95f9789fffd2df23ba355523
Sachin Chaudhary <sachinduhan1223@gmail.com>
2024-02-18 23:32:17 +0530

    Add new files and make code formatting changes

---
+++ components/TypographyInlineCode.tsx
@@ -0,0 +1,7 @@
+export function TypographyInlineCode() {
+  return (
+    <code className="relative rounded bg-neutral-800 text-white px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+      npx init hit-track
+    </code>
+  );
+}
